3bed7009fa6a39408a8a8116dffe48d1331bca17
Amrit Shokar <ashokar@tveon.com>
2021-05-12 13:37:37 -0700

    General controller clean up

--- src/controllers/GeneralController.ts
+++ src/controllers/GeneralController.ts
@@ -21,17 +21,13 @@ export class GeneralController {
     getTime(req: Request, res: Response) {
         let time = new Date().toDateString();
 
-        res.status(200).send(time);
+        return res.status(200).send(time);
     }
 
     async getWeather(req: Request, res: Response) {
         let weatherData: ClientResponse = await this.weatherService.fetchWeatherData();
         let response = weatherData.error ? weatherData.error : weatherData.data;
-        
-        // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-        // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        // res.setHeader('Access-Control-Allow-Credentials', true);
+
         return res.status(weatherData.statusCode).send(response);
     }
 
